a090274664884fc6741f6c4e39b3e802eda715b6
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-10-04 11:20:21 -0300

    Mapeamento

---
+++ Atividades_e_Trabalhos/Atividades_Individuais/js/projetos/orçamento/orcamento.js
@@ -0,0 +1,11 @@
+document.querySelector('.seu-nome').textContent ="Ana Amaral";
+
+const inputPaginas = document.querySelector("#qtd-paginas");
+const inputDesconto = document.querySelector("#desconto");
+const checkboxDesign = document.querySelector("#inclui-design");
+const inputPrazo = document.querySelector("#prazo-entrega");
+const resumoSubtotal = document.querySelector("#resumo-subtotal");
+const resumoAdicional = document.querySelector("#resumo-adicional");
+const resumoUrgencia = document.querySelector("#resumo-urgencia");
+const resumoDesconto = document.querySelector("#resumo-desconto");
+const resumoTotal = document.querySelector("#resumo-total");
