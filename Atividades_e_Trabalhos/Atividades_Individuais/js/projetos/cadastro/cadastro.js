ddf6fd6c3e0e563b7053f8dc22e0def268ceb414
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-10-11 11:59:53 -0300

    Cadastro Finalizado

--- Atividades_e_Trabalhos/Atividades_Individuais/js/projetos/cadastro/cadastro.js
+++ Atividades_e_Trabalhos/Atividades_Individuais/js/projetos/cadastro/cadastro.js
@@ -1,18 +1,24 @@
-const usuarios = JSON.parse(localStorage.getItem("cadastro_usuarios")) || [];
+document.querySelector.textContent = "Ana Amaral";
 
-const telaLista = document.querySelector("#tela-lista");
-const telaCadastro = document.querySelector("#tela-cadastro");
+let usuarios = JSON.parse(localStorage.getItem("cadastro_usuarios")) || [];
 
+//Telas
+const telaLista = document.querySelector
+("#tela-lista");
+const telaCadastro = document.querySelector
+("#tela-cadastro");
+
+
+//Botões
 const btnAdicionar = document.querySelector("#btn-adicionar");
 const btnVoltarLista = document.querySelector("#btn-voltar-lista");
 
-const form = document.querySelector("#user-form");
-const tabelaCorpo = document.querySelector("#user-table-body");
-
+// Inputs
+const inputId = document.querySelector("#user-id");
 const inputNome = document.querySelector("#user-nome");
 const inputSobrenome = document.querySelector("#user-sobrenome");
 const inputEmail = document.querySelector("#user-email");
-const inputCep = document.querySelector("#user-cep");
+const inputCEP = document.querySelector("#user-cep");
 const inputRua = document.querySelector("#user-rua");
 const inputNumero = document.querySelector("#user-numero");
 const inputComplemento = document.querySelector("#user-complemento");
@@ -21,71 +27,96 @@ const inputCidade = document.querySelector("#user-cidade");
 const inputEstado = document.querySelector("#user-estado");
 const inputObs = document.querySelector("#user-obs");
 
-function salvarStorage() {
-    localStorage.setItem("cadastro_usuarios", JSON.stringify(usuarios));
-}
+const form = document.querySelector("#user-form");
+const tabelaCorpo = document.querySelector("#user-table-body");
 
-function mostrarTelaLista() {
+
+//Funções
+function mostrarTelaLista(){
     telaLista.classList.remove("d-none");
     telaCadastro.classList.add("d-none");
     renderizarTabela();
+
 }
 
-function mostrarTelaCadastro() {
+function mostrarTelaCadastro(){
     telaCadastro.classList.remove("d-none");
     telaLista.classList.add("d-none");
 }
-
-function renderizarTabela() {
-    tabelaCorpo.innerHTML = "";
-
-    if (usuarios.length === 0) {
-        tabelaCorpo.innerHTML = `<tr><td colspan="3">Nenhum usuário cadastrado.</td></tr>`;
-        return;
-    }
-
-    usuarios.forEach(user => {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-            <td>${user.nome}</td>
-            <td>${user.sobrenome}</td>
-            <td>${user.email}</td>
-        `;
-        tabelaCorpo.appendChild(tr);
-    });
-}
-
-function salvarUsuario(event) {
-    event.preventDefault();
-
+function salvarUsuario(){
+    const id = Number(inputId.value);
+    const nome = inputNome.value;
+    const sobrenome = inputSobrenome.value;
+    const email = inputEmail.value;
+    const rua = inputRua.value;
+    const numero = inputNumero.value;
+    const complemento = inputComplemento.value;
+    const bairro = inputBairro.value;
+    const cidade = inputCidade.value;
+    const estado = inputEstado.value;
+    const obs = inputObs.value;
+    
     const usuario = {
-        id: Date.now(),
-        nome: inputNome.value,
-        sobrenome: inputSobrenome.value,
-        email: inputEmail.value,
-        cep: inputCep.value,
-        rua: inputRua.value,
-        numero: inputNumero.value,
-        complemento: inputComplemento.value,
-        bairro: inputBairro.value,
-        cidade: inputCidade.value,
-        estado: inputEstado.value,
-        obs: inputObs.value,
-    };
+        id: id || Date.now(), nome, sobrenome, email,
+        rua, numero, complemento, bairro, cidade, estado, obs
+    }
 
     usuarios.push(usuario);
     salvarStorage();
     mostrarTelaLista();
 }
 
-function inicializar() {
+
+function salvarStorage(){
+    localStorage.setItem("cadastro_usuarios", JSON.stringify(usuarios));
+}
+
+function renderizarTabela(){
+    tabelaCorpo.innerHTML = "";
+    usuarios.forEach(user => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${user.nome}</td>
+            <td>${user.sobrenome}</td>
+            <td>${user.email}</td>
+            <td>
+                <button type="button" class="btn btn-sm btn-warning"  data-id="${user.id}" >Editar</button>
+                <button type="button" class="btn btn-sm btn-danger" data-id="${user.id}">Excluir</button>
+            </td>
+        `;
+        tabelaCorpo.appendChild(tr);
+    });
+}
+
+function excluirUsuario(id){
+    if (confirm("Você deseja excluir esse usuário?")){
+        console.log(id);
+        usuarios = usuarios.filter(user => user.id !== id);
+        salvarStorage();
+        renderizarTabela();
+    }
+}
+
+function inicializar(){
     btnAdicionar.addEventListener("click", mostrarTelaCadastro);
     btnVoltarLista.addEventListener("click", mostrarTelaLista);
     form.addEventListener("submit", salvarUsuario);
-    
-    tabelaCorpo.addEventListener("click", (event) =>{
-        
+    mostrarTelaLista();
+
+    tabelaCorpo.addEventListener("click",(event) => {
+        const target = event.target.closest("button");
+        if(!target) return
+
+        const id = Number(target.dataset.id);
+
+        if (isNaN(id)) return
+
+        if (target.classList.contains("btn-warning")){
+            editarUsuario(id);
+        } else if (target.classList.contains("btn-danger")){
+            excluirUsuario(id);
+        }
     })
 }
 
-inicializar();
+inicializar()
