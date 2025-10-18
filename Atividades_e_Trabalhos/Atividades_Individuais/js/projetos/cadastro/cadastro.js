34f45ec40e88b47ffe60d1a499358e08f03abfc5
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-10-18 09:58:46 -0300

    Editar Usuário e Buscar CEP

--- Atividades_e_Trabalhos/Atividades_Individuais/js/projetos/cadastro/cadastro.js
+++ Atividades_e_Trabalhos/Atividades_Individuais/js/projetos/cadastro/cadastro.js
@@ -24,6 +24,9 @@ const inputObs = document.querySelector("#user-obs");
 
 const form = document.querySelector("#user-form");
 const tabelaCorpo = document.querySelector("#user-table-body");
+const btnCep = document.querySelector("#btn-buscar-cep");
+
+let idEdicao = null;
 
 
 function mostrarTelaLista() {
@@ -42,6 +45,7 @@ function salvarUsuario() {
     const nome = inputNome.value;
     const sobrenome = inputSobrenome.value;
     const email = inputEmail.value;
+    const cep = inputCep.value;
     const rua = inputRua.value;
     const numero = inputNumero.value;
     const complemento = inputComplemento.value;
@@ -51,11 +55,24 @@ function salvarUsuario() {
     const obs = inputObs.value;
 
     const usuario = {
-        id: id || Date.now(), nome, sobrenome, email, rua, numero, complemento, bairro, cidade, estado, obs
+        id: id || Date.now(), nome, sobrenome, email, cep, rua, numero, complemento, bairro, cidade, estado, obs
     }
 
-    usuarios.push(usuario);
+    if (idEdicao) { // se em edição, salvar as informações do usuário
+
+        const index = usuarios.findIndex(user => user.id === idEdicao); // 1 usuário = index 0
+        if (index !== -1) {
+            usuarios[index] = usuario;
+        }
+
+
+    } else { // se não, inserir novo usuário
+        usuarios.push(usuario);
+    }
+
     salvarNoStorage();
+    form.reset();
+    mostrarTelaLista;
 }
 
 function salvarNoStorage() {
@@ -91,9 +108,61 @@ function excluirUsuario(id) {
     }
 }
 
+function editarUsuario(id) {
+    const usuario = usuarios.find(user => user.id === id);
+
+    if (!usuario) return;
+
+    idEdicao = id;
+
+    inputId.value = usuario.id;
+    inputNome.value = usuario.nome;
+    inputSobrenome.value = usuario.sobrenome;
+    inputEmail.value = usuario.email;
+    inputCep.value = usuario.cep;
+    inputRua.value = usuario.rua;
+    inputNumero.value = usuario.numero;
+    inputComplemento.value = usuario.complemento;
+    inputBairro.value = usuario.bairro;
+    inputCidade.value = usuario.cidade;
+    inputEstado.value = usuario.estado;
+    inputObs.value = usuario.obs;
+
+    mostrarTelaCadastro();
+}
+
+async function buscarCEP() {
+    const cep = inputCep.value.replace(/\D/g,"");
+    if (cep.length === 8) {
+
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+        const dados = await resposta.json();
+
+        console.log(dados);
+
+        if (!dados.erro) {
+            inputCep.value = dados.cep;
+            inputRua.value = dados.logradouro;
+            inputComplemento.value = dados.complemento;
+            inputBairro.value = dados.bairro;
+            inputCidade.value = dados.localidade;
+            inputEstado.value = dados.estado;
+        }
+        else{
+            alert("CEP Inválido, tente novamente!")
+        }
+
+    } else {
+        alert("Verifique a quantidade de números digitados!")
+    }
+}
+
 function inicializar() {
     btnAdicionar.addEventListener("click", mostrarTelaCadastro);
     btnVoltarLista.addEventListener("click", mostrarTelaLista);
+    btnCep.addEventListener("click", buscarCEP);
+
     form.addEventListener("submit", salvarUsuario);
     mostrarTelaLista();
 
